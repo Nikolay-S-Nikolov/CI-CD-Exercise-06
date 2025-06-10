6961f146c036790c330a4a7014b6e32cfeca218b
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2025-06-10 13:02:35 +0300

    run Chrome headless

--- TestProject3/DropDown.cs
+++ TestProject3/DropDown.cs
@@ -15,8 +15,16 @@ namespace TestProject3
         [SetUp]
         public void SetUp()
         {
-            // Create object of ChromeDriver
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");
+            options.AddArgument("no-sandbox");
+            options.AddArgument("disable-dev-shm-usage");
+            options.AddArgument("disable-gpu");
+            options.AddArgument("window-size=1920x1080");
+            options.AddArgument("dissable-extensions");
+            options.AddArgument("remote-debugging-port=9222");
+
+            driver = new ChromeDriver(options);
 
             // Add implicit wait
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
